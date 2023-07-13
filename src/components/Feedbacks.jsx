9eb5b507a5eafc598f298d7ff66fbb1a371c8dd5
Yichao <liuyichao200129@gmail.com>
2023-07-13 17:47:39 +0200

    fix

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -49,10 +49,10 @@ const Feedbacks = () => {
       <div
         className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
       >
-        <motion.div variants={textVariant()}>
+        <div variants={textVariant()}>
           <p className={styles.sectionSubText}>What others say</p>
-          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
-        </motion.div>
+          <h2 className={styles.sectionHeadText}>Testimonials</h2>
+        </div>
       </div>
       <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
         {testimonials.map((testimonial, index) => (
